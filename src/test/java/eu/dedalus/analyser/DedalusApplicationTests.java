c814a0b043419944c07207cc5e304bced94e74f6
nemch <Please098123!>
2021-09-23 00:43:32 +0300

    Initial commit

---
+++ src/test/java/eu/dedalus/analyser/DedalusApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dedalus.analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedalusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
